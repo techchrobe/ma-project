54bcce33389f8ce92af760118c0e0067c0415658
Christina Obereigner <chrisi.obereigner@gmail.com>
2023-03-07 18:06:39 +0100

    Add A* code to actual level generator

--- LidarTest/Assets/Scripts/LevelGenerator.cs
+++ LidarTest/Assets/Scripts/LevelGenerator.cs
@@ -10,6 +10,7 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] GameObject simplePlatform;
     [SerializeField] GameObject debugObj;
     [SerializeField] GameObject goal;
+    [SerializeField] float stepDistance = 0.8f;
 
     private List<GameObject> centers = new List<GameObject>();
     private float playerJumpHeight = 0f;
@@ -18,6 +19,19 @@ public class LevelGenerator : MonoBehaviour
     private Vector3 endPosition;
 
     public Vector3 StartPosition { get => startPosition; }
+
+    enum Direction {
+        Top = 0,
+        Right = 1,
+        Bottom = 2,
+        Left = 3,
+        TopLeft = 4,
+        TopRight = 5,
+        BottomLeft = 6,
+        BottomRight = 7
+    };
+
+    Direction[] All = { Direction.Top, Direction.Right, Direction.Bottom, Direction.Left, Direction.TopLeft, Direction.TopRight, Direction.BottomLeft, Direction.BottomRight };
 
     public void Init(GameObject cam, ARMeshManager manager, float jumpHeight)
     {
@@ -62,51 +76,122 @@ public class LevelGenerator : MonoBehaviour
         Instantiate(goal, endPosition + new Vector3(0, 0.1f, 0), transform.rotation);
 
         // Place platforms between start and end position
-        int platformCount = 0;
-        Vector3 lastPlatformPosition = startPosition;
-        while (Vector3.Distance(lastPlatformPosition, endPosition) > playerJumpHeight && platformCount < 20)
-        {
-            Vector3 dirToGoal = (endPosition - lastPlatformPosition).normalized;
+        // Find path between Start and End position
+        List<NodeRecord> open = new List<NodeRecord>();
+        List<NodeRecord> closed = new List<NodeRecord>();
 
-            float xPos = Random.Range(0.1f, playerJumpHeight) * (Random.Range(0, 2) == 0 ? 1 : -1);
-            float yPos = Random.Range(0.1f, playerJumpHeight) * (Random.Range(0, 2) == 0 ? 1 : -1);
-            float zPos = Random.Range(0.1f, playerJumpHeight) * (Random.Range(0, 2) == 0 ? 1 : -1);
+        open.Add(new NodeRecord(new Node(startPosition, null), null, 0, Vector3.Distance(startPosition, endPosition)));
+        NodeRecord current = open[0];
+        while(open.Count != 0) {
+            current = open[0];
 
-            if ((dirToGoal.x < 0 && xPos > 0)
-                || (dirToGoal.x > 0 && xPos < 0))
-            {
-                xPos *= -1;
-            }
-            if ((dirToGoal.y < 0 && yPos > 0)
-                || (dirToGoal.y > 0 && yPos < 0))
-            {
-                yPos *= -1;
-            }
-            if ((dirToGoal.z < 0 && zPos > 0)
-                || (dirToGoal.z > 0 && zPos < 0))
-            {
-                zPos *= -1;
+            // if current node is close enuogh to the goal stop
+            if(Vector3.Distance(current.Node.getPosition(), endPosition) <= stepDistance) {
+                break;
             }
 
-            Vector3 newPlatformPosition = new Vector3(xPos, yPos, zPos) + lastPlatformPosition + (dirToGoal / 50);
-            // Don't place to close to ceiling
-            float boundDistance = DistanceToCeiling(newPlatformPosition);
-            if (boundDistance < 0.7f)
-            {
-                newPlatformPosition.y = newPlatformPosition.y - (0.7f - boundDistance);
-            }
+            // add neighbours
+            foreach(Direction d in All) {
+                int cost = 1;
+                if((int)d >= 4) {
+                    cost = 2;
+                }
 
-            // Don't place to close to ground
-            boundDistance = DistanceToGround(newPlatformPosition);
-            if (boundDistance < 0.1f)
-            {
-                newPlatformPosition.y = newPlatformPosition.y - (boundDistance - 0.1f);
-            }
+                Node neighbour = GetNeighbour(d, current.Node);
+                if(IsNodeValid(neighbour)) {
 
-            Instantiate(simplePlatform, newPlatformPosition, transform.rotation);
-            lastPlatformPosition = newPlatformPosition;
-            platformCount++;
+                    NodeRecord isInClosed = QueueContainsNode(closed, neighbour);
+                    NodeRecord isInOpen = QueueContainsNode(open, neighbour);
+
+                    float endNodeCost = current.CostSoFar + cost;
+                    float endNodeHeuristic = 0.0f;
+
+                    NodeRecord record;
+                    if(isInClosed != null) {
+                        record = isInClosed;
+                        if(record.CostSoFar <= endNodeCost)
+                            continue;
+
+                        closed.Remove(record);
+                        endNodeHeuristic = record.EstimatedTotalCost - record.CostSoFar;
+                    }
+                    else if(isInOpen != null) {
+                        record = isInOpen;
+                        if(record.CostSoFar <= endNodeCost)
+                            continue;
+                        endNodeHeuristic = record.EstimatedTotalCost - record.CostSoFar;
+                    }
+                    else {
+                        record = new NodeRecord(neighbour, current, endNodeCost, endNodeHeuristic);
+                        endNodeHeuristic = Vector3.Distance(neighbour.getPosition(), endPosition);
+                    }
+
+                    // update node record
+                    record.CostSoFar = endNodeCost;
+                    record.Connection = current;
+                    record.EstimatedTotalCost = endNodeCost + endNodeHeuristic;
+
+                    // reinsert to trigger sort
+                    open.Add(record);
+                    open.Sort();
+
+                }
+                open.Remove(current);
+                closed.Add(current);
+            }
         }
+
+        // placce platforms on path
+        while(current.Node.getPosition() != startPosition) {
+            Instantiate(simplePlatform, current.Node.getPosition(), transform.rotation);
+            current = current.Connection;
+        }
+    }
+
+    NodeRecord QueueContainsNode(List<NodeRecord> list, Node n) {
+        foreach(NodeRecord nr in list) {
+            if(nr.Node.getPosition() == n.getPosition())
+                return nr;
+        }
+        return null;
+    }
+
+    Node GetNeighbour(Direction direction, Node lastPosition) {
+        switch(direction) {
+            case Direction.Top:
+                return new Node(lastPosition.getPosition() + new Vector3(0, 0, stepDistance), lastPosition);
+            case Direction.TopRight:
+                return new Node(lastPosition.getPosition() + new Vector3(stepDistance, 0, stepDistance), lastPosition);
+            case Direction.Right:
+                return new Node(lastPosition.getPosition() + new Vector3(stepDistance, 0, 0), lastPosition);
+            case Direction.BottomRight:
+                return new Node(lastPosition.getPosition() + new Vector3(stepDistance, 0, -stepDistance), lastPosition);
+            case Direction.Bottom:
+                return new Node(lastPosition.getPosition() + new Vector3(0, 0, -stepDistance), lastPosition);
+            case Direction.BottomLeft:
+                return new Node(lastPosition.getPosition() + new Vector3(-stepDistance, 0, -stepDistance), lastPosition);
+            case Direction.Left:
+                return new Node(lastPosition.getPosition() + new Vector3(-stepDistance, 0, 0), lastPosition);
+            case Direction.TopLeft:
+                return new Node(lastPosition.getPosition() + new Vector3(-stepDistance, 0, stepDistance), lastPosition);
+        }
+        return lastPosition;
+    }
+
+    bool IsNodeValid(Node n) {
+        RaycastHit hit;
+        if(Physics.SphereCast(n.getPosition(), 0.1f, Vector3.down, out hit)) {
+
+            // move platform a bit ot the side if it's to close to a wall
+            if(Physics.Raycast(n.getPosition(), Vector3.left, 0.35f)
+                || Physics.Raycast(n.getPosition(), Vector3.right, 0.35f)
+                || Physics.Raycast(n.getPosition(), Vector3.forward, 0.35f)
+                || Physics.Raycast(n.getPosition(), Vector3.back, 0.35f)) {
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 
     private float DistanceToGround(Vector3 position)
