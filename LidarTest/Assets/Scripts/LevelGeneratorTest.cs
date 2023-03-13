d706cf4512ff5f1f514cbec1e07449f7e6cc23f0
Christina Obereigner <chrisi.obereigner@gmail.com>
2023-03-13 16:11:41 +0100

    Fix Astar infinity loop

--- LidarTest/Assets/Scripts/LevelGeneratorTest.cs
+++ LidarTest/Assets/Scripts/LevelGeneratorTest.cs
@@ -33,98 +33,9 @@ public class LevelGeneratorTest : MonoBehaviour
     public void Start()
     {
         Vector3 end = FindEndPosition(startPosition.transform.position);
+        if(end == startPosition.transform.position)
+            return;
         AStar(startPosition.transform.position, end);
-        //Vector3 end = endPosition.transform.position;
-
-        // Find path between Start and End position
-        //List<NodeRecord> open = new List<NodeRecord>();
-        //List<NodeRecord> closed = new List<NodeRecord>();
-
-        //open.Add(new NodeRecord(new Node(startPosition.transform.position, null), null, 0, Vector3.Distance(startPosition.transform.position, end)));
-        //NodeRecord current = open[0];
-        //while(open.Count != 0) {
-        //    current = open[0];
-
-        //    // if current node is close enough to the goal stop
-        //    if(Vector3.Distance(current.Node.Position, end) <= stepDistance) {
-        //        break;
-        //    }
-
-        //    // add neighbours
-        //    foreach(Direction d in All) {
-        //        int cost = 1;
-        //        if((int)d >= 4) {
-        //            cost = 2;
-        //        }
-
-        //        Node neighbour = GetNeighbour(d, current.Node);
-        //        if(IsNodeValid(neighbour)) {
-
-        //            NodeRecord isInClosed = QueueContainsNode(closed, neighbour);
-        //            NodeRecord isInOpen = QueueContainsNode(open, neighbour);
-
-        //            float endNodeCost = current.CostSoFar + cost;
-        //            float endNodeHeuristic = 0.0f;
-
-        //            NodeRecord record;
-        //            if(isInClosed != null) {
-        //                record = isInClosed;
-        //                if(record.CostSoFar <= endNodeCost)
-        //                    continue;
-
-        //                closed.Remove(record);
-        //                endNodeHeuristic = record.EstimatedTotalCost - record.CostSoFar;
-        //            }
-        //            else if(isInOpen != null) {
-        //                record = isInOpen;
-        //                if(record.CostSoFar <= endNodeCost)
-        //                    continue;
-        //                endNodeHeuristic = record.EstimatedTotalCost - record.CostSoFar;
-        //            }
-        //            else {
-        //                record = new NodeRecord(neighbour, current, endNodeCost, endNodeHeuristic);
-        //                endNodeHeuristic = Vector3.Distance(neighbour.Position, endPosition.transform.position);
-        //            }
-
-        //            // update node record
-        //            record.CostSoFar = endNodeCost;
-        //            record.Connection = current;
-        //            record.EstimatedTotalCost = endNodeCost + endNodeHeuristic;
-
-        //            // reinsert to trigger sort
-        //            open.Add(record);
-        //            open.Sort();
-
-        //        }
-        //        open.Remove(current);
-        //        closed.Add(current);
-        //    }
-        //}
-
-        //// placce platforms on path
-        //Vector3 lastPosition = startPosition.transform.position;
-        //while(current.Node.Position != startPosition.transform.position) {
-        //    Vector3 platformPosition = current.Node.FixedPosition;
-
-        //    // set y position
-        //    float yPos = Random.Range(-0.4f, 0.4f);
-        //    platformPosition.y = lastPosition.y + yPos;
-
-        //    float groundDistance = DistanceToGround(platformPosition);
-
-        //    if(groundDistance == float.MaxValue) {
-        //        platformPosition.y = lastPosition.y + (yPos * -1);
-        //        groundDistance = DistanceToGround(platformPosition);
-        //    }
-        //    // Don't place to close to ground
-        //    if(groundDistance < 0.1f) {
-        //        platformPosition.y = platformPosition.y - (groundDistance - 0.1f);
-        //    }
-
-        //    Instantiate(simplePlatform, platformPosition, transform.rotation);
-        //    lastPosition = platformPosition;
-        //    current = current.Connection;
-        //}
     }
 
     private void AStar(Vector3 startPosition, Vector3 endPosition) {
@@ -137,7 +48,7 @@ public class LevelGeneratorTest : MonoBehaviour
             current = open[0];
 
             // if current node is close enough to the goal stop
-            if(Vector3.Distance(current.Node.Position, endPosition) <= stepDistance) {
+            if(Vector3.Distance(current.Node.Position, endPosition) <= stepDistance + distanceToWall) {
                 break;
             }
 
@@ -187,9 +98,9 @@ public class LevelGeneratorTest : MonoBehaviour
                     open.Sort();
 
                 }
-                open.Remove(current);
-                closed.Add(current);
             }
+            open.Remove(current);
+            closed.Add(current);
         }
 
         // placce platforms on path
